ba39666779e4372db031a2a17e08299af46f91a5
Afridi Azmain <Azmain951>
2022-04-03 22:53:31 +0600

    react router installed and header component created and style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <ProductDetails></ProductDetails>
     </div>
   );
