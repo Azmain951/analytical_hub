43ab7382d0d1231c36f8f636c785f04d342ab3bf
Afridi Azmain <Azmain951>
2022-04-04 16:27:25 +0600

    minor modifications

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <h1>This is About Page</h1>
+            <h2>To be completed Later........</h2>
+        </div>
+    );
+};
+
+export default About;
